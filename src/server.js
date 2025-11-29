240520b8e99ac1ba8a58570d94260bf35a2f1372
Aman kumar <117091469+Amanking2425@users.noreply.github.com>
2025-11-29 19:29:01 +0530

    Update CORS configuration to allow Vercel frontend

--- src/server.js
+++ src/server.js
@@ -59,7 +59,11 @@ const __dirname = path.resolve();
 
 // ✅ ✅ ✅ ESSENTIAL CHANGE #1 (ALLOW VERCEL FRONTEND)
 app.use(cors({
-  origin: "http://localhost:5173",   // ✅ allow your frontend
+  //origin: "http://localhost:5173",   // ✅ allow your frontend
+ origin: [
+    "http://localhost:5173", 
+    "https://quick-talk-frontend.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
